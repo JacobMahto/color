e537882a5fd1ed8d3173cdd58bdd7385b8ffdd04
jvm <jacobmahto148@gmail.com>
2019-06-21 15:36:05 +0530

    update

--- content.php
+++ content.php
@@ -16,7 +16,13 @@ $rsl=query($query);
 <div class="col bg-light">
 <!-- RIGHT PANE --!>
 <div class="row justify-content-around">
-<video-js id="vid1" width="450" style="border:0.2px solid blue;" loop  controls preload="metadata" data-setup='{}'>
+<!--IDEAL FORMAT IF DATA INTIALISATION HAS TO BE DONE FROM OUTER TAGS (AT THE END OF THE PAGE)
+<video-js id="kvid1" width="450" style="border:0.2px solid blue;" loop  controls preload="metadata" data-setup='{}'>
+<source src="/color/assets/vid1.mp4" type="video/mp4">
+</video-js>
+--!>
+<!-- IDEAL SETTING IF DATA INTIALISATION HAS TO BE DONE INSIDE THE VIDEO....IDEAL FOR PHP-->
+<video-js id="vid2" width="450" style="border:0.2px solid blue;"  loop controls preload="metadata" data-setup='{"playbackRates":[0.5,1,1.5,2],"plugins":{"abLoopPlugin":{"start":50,"end":55,"enabled":false,"loopIfBeforeStart":false,"loopIfAfterEnd":true,"pauseAfterLooping":false,"pauseBeforeLooping":false,"createButtons":true}},"userActions":{"doubleClick":true},"loop":true}'>
 <source src="/color/assets/vid1.mp4" type="video/mp4">
 </video-js>
 </div>
@@ -28,20 +34,21 @@ $rsl=query($query);
 </body>
 <script>
 //initialise the video with the plugin and initial settings
-var video = videojs("vid1",{
-	playbackRates: [0.5, 1, 1.5, 2],userActions:{doubleClick:true},loop:true,
-plugins: {
-		abLoopPlugin: {
-			start:50    	//in seconds - defaults to 0
-			,end:55    	//in seconds. Set to  false to loop to end of video. Defaults to false
-			,enabled:false			//defaults to false
-			,loopIfBeforeStart:false //allow video to play normally before the loop section? defaults to true
-			,loopIfAfterEnd:true	// defaults to true
-			,pauseAfterLooping: false     	//if true, after looping video will pause. Defaults to false
-			,pauseBeforeLooping: false     	//if true, before looping video will pause. Defaults to false
-			,createButtons: true		//defaults to true
-		}
-	}
+//NOT USED. BUT CAN BE USED IF VIDEOJS TAG INTIALISATION HAS TO BE DONE USING "ID" TAG , FROM OUTSIDE.
+var video1 = videojs("vid1",{
+playbackRates: [0.5, 1, 1.5, 2],userActions:{doubleClick:true},loop:true,
+	plugins: {
+	abLoopPlugin: {
+	start:50    	//in seconds - defaults to 0
+		,end:55    	//in seconds. Set to  false to loop to end of video. Defaults to false
+		,enabled:false			//defaults to false
+		,loopIfBeforeStart:false //allow video to play normally before the loop section? defaults to true
+		,loopIfAfterEnd:true	// defaults to true
+		,pauseAfterLooping: false     	//if true, after looping video will pause. Defaults to false
+		,pauseBeforeLooping: false     	//if true, before looping video will pause. Defaults to false
+		,createButtons: true		//defaults to true
+}
+}
 });
-
+video1.currentTime(120);
 </script>

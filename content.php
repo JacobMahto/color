52e86e2f7db1f66e36f46739239c73633efa29cb
jvm <jacobmahto148@gmail.com>
2019-06-24 16:12:43 +0530

    added functionality for content rendering

--- content.php
+++ content.php
@@ -2,19 +2,30 @@
 include("includes/header.php");
 include("includes/nav.php");
 include("functions/db.php");
+$compensanate="/color";
 //get class list
 if(isset($_GET['std_code']) && isset($_GET['sub_code'])){
 	$std_code=$_GET['std_code'];
 	$sub_code=$_GET['sub_code'];
 	//Getting Side Pane items
-	$query="SELECT heads.head,topic,path FROM files,heads WHERE sub_code=$std_code AND sub_code=$sub_code AND heads.sn=files.head GROUP BY topic;";	
+	$query="SELECT DISTINCT files.head,heads.head FROM heads,files WHERE heads.sn=files.head AND std_code=$std_code AND sub_code=$sub_code;";	
 	$rsl1=query($query);
-	$now='';
-	$prev='';
-	for($now='';$now!="BREAK";$now){
-		$while($r=fetch_array($rsl1)){
-		$now=$r['heads'];
-	}}
+	$html="";
+	while($r1=fetch_array($rsl1)){
+		$head_code=$r1[0];
+		$head_name=$r1[1];
+		$html.=<<<DELIMITER
+<div class="row bg-danger text-light">$head_name</div>
+DELIMITER;
+		$query="SELECT DISTINCT topic,path FROM heads,files WHERE files.head=$head_code AND std_code=$std_code AND sub_code=$sub_code;";
+		$rsl2=query($query);
+		while($r2=fetch_array($rsl2)){
+			$html.=<<<DELIMITER
+<a href="$compensanate/content.php?std_code=$std_code&sub_code=$sub_code&head_code=$head_code&topic=$r2[0]" class="row">$r2[0]</a>
+DELIMITER;
+		}
+	}
+
 
 }
 else{
@@ -22,15 +33,39 @@ else{
 }
 ?>
 <body>
-<div class=" container">
+<div class=" container-fluid">
 <div class="row">
 <div class="col-3">
 <!-- LEFT PANE --!>
-
+<?php
+echo $html;
+?>
 </div>
 <div class="col bg-light">
 <!-- RIGHT PANE --!>
 <div class="row justify-content-around">
+<?php
+if(isset($_GET['std_code']) && isset($_GET['sub_code']) && isset($_GET['head_code']) && isset($_GET['topic'])){
+	$head_code=$_GET['head_code'];
+	$sub_code=$_GET['sub_code'];
+	$std_code=$_GET['std_code'];
+	$topic=$_GET['topic'];
+	$query="SELECT path FROM files WHERE topic='$topic' AND head=$head_code AND std_code=$std_code AND sub_code=$sub_code;";
+	$rsl3=query($query);
+	echo $query;
+	while($r3=fetch_array($rsl3)){
+		echo $r3[0];	
+		$html=<<<DELIMITER
+
+<!-- IDEAL SETTING IF DATA INTIALISATION HAS TO BE DONE INSIDE THE VIDEO....IDEAL FOR PHP-->
+<video-js id="vid2" width="450" style="border:0.2px solid blue;"  loop controls preload="metadata" data-setup='{"playbackRates":[0.5,1,1.5,2],"plugins":{"abLoopPlugin":{"start":50,"end":55,"enabled":false,"loopIfBeforeStart":false,"loopIfAfterEnd":true,"pauseAfterLooping":false,"pauseBeforeLooping":false,"createButtons":true}},"userActions":{"doubleClick":true},"loop":true}'>
+<source src="/color/$r3[0]" type="video/mp4">
+</video-js>
+DELIMITER;
+		echo $html;
+	}
+}
+?>
 <!--IDEAL FORMAT IF DATA INTIALISATION HAS TO BE DONE FROM OUTER TAGS (AT THE END OF THE PAGE)
 <video-js id="kvid1" width="450" style="border:0.2px solid blue;" loop  controls preload="metadata" data-setup='{}'>
 <source src="/color/assets/vid1.mp4" type="video/mp4">

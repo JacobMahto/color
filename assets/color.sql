deb84ced50bfd0721b3cbff589cae1deadaf2fb2
jvm <jacobmahto148@gmail.com>
2019-07-28 23:28:01 +0530

    update

--- assets/color.sql
+++ assets/color.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 14, 2019 at 09:41 PM
+-- Generation Time: Jul 28, 2019 at 07:56 PM
 -- Server version: 10.1.40-MariaDB
 -- PHP Version: 7.3.5
 
@@ -129,7 +129,83 @@ INSERT INTO `files` (`sn`, `std_code`, `sub_code`, `head`, `topic`, `type`, `nam
 (88, 6, 5, 10, 'Universe', 'video', 'Solar System', '/assets/6/SST/geo/solarSystem.mp4', NULL, NULL),
 (89, 6, 5, 11, 'India', 'video', 'Dresses', '/assets/6/SST/civics/dresses.mp4', NULL, NULL),
 (90, 6, 5, 11, 'India', 'video', 'Dances', '/assets/6/SST/civics/dances.mp4', NULL, NULL),
-(91, 6, 5, 11, 'India', 'video', 'Festivals', '/assets/6/SST/civics/festivals.mp4', NULL, NULL);
+(91, 6, 5, 11, 'India', 'video', 'Festivals', '/assets/6/SST/civics/festivals.mp4', NULL, NULL),
+(92, 10, 4, 14, 'Life Processes', 'video', 'Anaerobic Respiration', '/assets/10/Science/bio/anaerobicRespiration.mp4', NULL, NULL),
+(93, 10, 4, 14, 'Life Processes', 'video', 'Muscle Respirations', '/assets/10/Science/bio/anaerobicRespirationMuscles.mp4', NULL, NULL),
+(94, 10, 4, 14, 'Life Processes', 'video', 'Digestion', '/assets/10/Science/bio/digestion.mp4', NULL, NULL),
+(95, 10, 4, 14, 'Life Processes', 'video', 'Respiration', '/assets/10/Science/bio/respiration.mp4', NULL, NULL),
+(96, 10, 4, 14, 'Reproduction', 'video', 'Sexual Reproduction in Humans', '/assets/10/Science/bio/sexualReproductionHumans.mp4', NULL, NULL),
+(97, 10, 4, 14, 'Reproduction', 'video', 'Reproduction in Plants', '/assets/10/Science/bio/reproductionPlants.mp4', NULL, NULL),
+(98, 10, 4, 14, 'Control And Coordination', 'video', 'Neurons , Synapses and Nerve cells', '/assets/10/Science/bio/nerveCells-Neurons-Synapses.mp4', NULL, NULL),
+(99, 10, 4, 14, 'Genetics', 'video', 'Introdution to Genetics', '/assets/10/Science/bio/genetics.mp4', NULL, NULL),
+(100, 10, 4, 14, 'Genetics', 'video', 'Mendel and Genetic Crosses', '/assets/10/Science/bio/mendelAndGeneticCrosses.mp4', NULL, NULL),
+(101, 10, 4, 13, 'Organic Chemistry', 'video', 'Carbon Atom and characteristics', '/assets/10/Science/chem/carbonAtomCharacteristics.mp4', NULL, NULL),
+(102, 10, 4, 13, 'Organic Chemistry', 'video', 'Classification of Hydrocarbons', '/assets/10/Science/chem/hydrocarbonClassification.mp4', NULL, NULL),
+(103, 10, 4, 13, 'Organic Chemistry', 'video', 'IUPAC Rule', '/assets/10/Science/chem/iupac.mp4', NULL, NULL),
+(104, 10, 4, 13, 'Organic Chemistry', 'video', 'IUPAC Rule for cyclic compounds', '/assets/10/Science/chem/iupacCyclic.mp4', NULL, NULL),
+(105, 10, 4, 13, 'Acid and Bases', 'video', 'Bronsted Lowry Concept', '/assets/10/Science/chem/bronstedLowryConcept.mp4', NULL, NULL),
+(106, 10, 4, 13, 'Acid and Bases', 'video', 'Lewis Acid and Base', '/assets/10/Science/chem/lewisAcidBase.mp4', NULL, NULL),
+(107, 10, 4, 13, 'Acid and Bases', 'video', 'Neutralization', '/assets/10/Science/chem/neutralization.mp4', NULL, NULL),
+(108, 10, 4, 13, 'Acid and Bases', 'video', 'PH Scale', '/assets/10/Science/chem/phScale.mp4', NULL, NULL),
+(109, 10, 4, 13, 'Chemical Reactions', 'video', 'Chemical and Physical Changes', '/assets/10/Science/chem/chemicalPhysicalChanges.mp4', NULL, NULL),
+(110, 10, 4, 13, 'Chemical Reactions', 'video', 'Chemical Reactions and Types', '/assets/10/Science/chem/chemicalReactionsAndTypes.mp4', NULL, NULL),
+(111, 10, 4, 13, 'Chemical Reactions', 'video', 'Useful Compounds', '/assets/10/Science/chem/usefulCompound.mp4', NULL, NULL),
+(112, 10, 4, 13, 'Structure of Atom', 'video', 'Dalton Atomic Theory', '/assets/10/Science/chem/daltonAtomicTheory.mp4', NULL, NULL),
+(113, 10, 4, 13, 'Structure of Atom', 'video', 'Rutherford Experiment', '/assets/10/Science/chem/rutherford.mp4', NULL, NULL),
+(114, 10, 4, 13, 'Structure of Atom', 'video', 'Thomson Model', '/assets/10/Science/chem/thomsonModel.mp4', NULL, NULL),
+(115, 10, 4, 13, 'Periodicity', 'video', 'Modern Periodic Table', '/assets/10/Science/chem/modernPeriodicTable.mp4', NULL, NULL),
+(116, 10, 4, 13, 'Chemical Reactions', 'video', 'Catalyst', '/assets/10/Science/chem/catalyst.mp4', NULL, NULL),
+(117, 10, 4, 13, 'Periodicity', 'video', 'Electronegativity', '/assets/10/Science/chem/electronegativity.mp4', NULL, NULL),
+(118, 10, 1, 6, 'Noun', 'video', 'Noun', '/assets/10/English/gr/noun.mp4', NULL, NULL),
+(119, 10, 1, 6, 'Noun', 'video', 'Collective and Abstract Noun', '/assets/10/English/gr/collectiveAndAbstractNoun.mp4', NULL, NULL),
+(120, 10, 1, 6, 'Reported Speech', 'video', 'Direct and Indirect Speech', '/assets/10/English/gr/directIndirectSpeech.mp4', NULL, NULL),
+(121, 10, 1, 6, 'Pronoun', 'video', 'Introduction', '/assets/10/English/gr/pronoun.mp4', NULL, NULL),
+(122, 10, 1, 6, 'Conjunction', 'video', 'Introduction', '/assets/10/English/gr/conjunction.mp4', NULL, NULL),
+(123, 10, 1, 15, 'Letter', 'video', 'Formal Letter', '/assets/10/English/writing/formalLetter.mp4', NULL, NULL),
+(124, 10, 1, 15, 'Letter', 'video', 'Informal Letter', '/assets/10/English/writing/formalLetter.mp4', NULL, NULL),
+(125, 10, 1, 15, 'Notice', 'video', 'Introduction', '/assets/10/English/writing/notice.mp4', NULL, NULL),
+(126, 9, 5, 11, 'Democracy', 'video', 'Democracy Concept', '/assets/9/SST/civics/democracy.mp4', NULL, NULL),
+(127, 9, 5, 11, 'Democracy', 'video', 'Democracy in India', '/assets/9/SST/civics/democracyInIndia.mp4', NULL, NULL),
+(128, 9, 5, 10, 'Soil', 'video', 'Types of Soil', '/assets/9/SST/geo/typesOfSoil.mp4', NULL, NULL),
+(129, 9, 5, 9, 'Unification', 'video', 'Unification of Italy', '/assets/9/SST/history/unificationOfItaly.mp4', NULL, NULL),
+(130, 7, 1, 6, 'Voice', 'video', 'Active and Passive Voice', '/assets/7/English/gr/activePassiveVoice.mp4', NULL, NULL),
+(131, 7, 1, 6, 'Degree of Comparison', 'video', 'Introduction', '/assets/7/English/gr/degreeOfComparison.mp4', NULL, NULL),
+(132, 7, 1, 6, 'Reported Speech', 'video', 'Direct and Indirect Speech', '/assets/7/English/gr/directIndirectSpeech.mp4', NULL, NULL),
+(133, 6, 1, 6, 'Noun', 'video', 'Noun', '/assets/10/English/gr/noun.mp4', NULL, NULL),
+(134, 6, 1, 6, 'Noun', 'video', 'Collective and Abstract Noun', '/assets/10/English/gr/collectiveAndAbstractNoun.mp4', NULL, NULL),
+(135, 6, 1, 6, 'Reported Speech', 'video', 'Direct and Indirect Speech', '/assets/10/English/gr/directIndirectSpeech.mp4', NULL, NULL),
+(136, 6, 1, 6, 'Pronoun', 'video', 'Introduction', '/assets/10/English/gr/pronoun.mp4', NULL, NULL),
+(137, 6, 1, 6, 'Conjunction', 'video', 'Introduction', '/assets/10/English/gr/conjunction.mp4', NULL, NULL),
+(138, 6, 1, 15, 'Letter', 'video', 'Formal Letter', '/assets/10/English/writing/formalLetter.mp4', NULL, NULL),
+(139, 6, 1, 15, 'Letter', 'video', 'Informal Letter', '/assets/10/English/writing/formalLetter.mp4', NULL, NULL),
+(140, 6, 1, 15, 'Notice', 'video', 'Introduction', '/assets/10/English/writing/notice.mp4', NULL, NULL),
+(141, 6, 1, 6, 'Voice', 'video', 'Active and Passive Voice', '/assets/7/English/gr/activePassiveVoice.mp4', NULL, NULL),
+(142, 6, 1, 6, 'Degree of Comparison', 'video', 'Introduction', '/assets/7/English/gr/degreeOfComparison.mp4', NULL, NULL),
+(143, 6, 1, 6, 'Reported Speech', 'video', 'Direct and Indirect Speech', '/assets/7/English/gr/directIndirectSpeech.mp4', NULL, NULL),
+(148, 8, 1, 6, 'Noun', 'video', 'Noun', '/assets/10/English/gr/noun.mp4', NULL, NULL),
+(149, 8, 1, 6, 'Noun', 'video', 'Collective and Abstract Noun', '/assets/10/English/gr/collectiveAndAbstractNoun.mp4', NULL, NULL),
+(150, 8, 1, 6, 'Narration', 'video', 'Direct and Indirect Speech', '/assets/10/English/gr/directIndirectSpeech.mp4', NULL, NULL),
+(151, 8, 1, 6, 'Pronoun', 'video', 'Introduction', '/assets/10/English/gr/pronoun.mp4', NULL, NULL),
+(152, 8, 1, 6, 'Conjunction', 'video', 'Introduction', '/assets/10/English/gr/conjunction.mp4', NULL, NULL),
+(153, 8, 1, 15, 'Letters', 'video', 'Formal Letter', '/assets/10/English/writing/formalLetter.mp4', NULL, NULL),
+(154, 8, 1, 15, 'Letter', 'video', 'Informal Letter', '/assets/10/English/writing/formalLetter.mp4', NULL, NULL),
+(155, 8, 1, 15, 'Notice', 'video', 'Introduction', '/assets/10/English/writing/notice.mp4', NULL, NULL),
+(156, 8, 1, 6, 'Voice', 'video', 'Active and Passive Voice', '/assets/7/English/gr/activePassiveVoice.mp4', NULL, NULL),
+(157, 8, 1, 6, 'Degree of Comparison', 'video', 'Introduction', '/assets/7/English/gr/degreeOfComparison.mp4', NULL, NULL),
+(158, 8, 1, 6, 'Reported Speech', 'video', 'Direct and Indirect Speech', '/assets/7/English/gr/directIndirectSpeech.mp4', NULL, NULL),
+(163, 151, 102, 3, 'Patriotic', 'video', 'Jana Gana Mana', '/assets/Music/Piano/janGanMan.mp4', NULL, NULL),
+(164, 151, 102, 3, 'Patriotic', 'video', 'Vande Matram', '/assets/Music/Piano/vandeMatram.mp4', NULL, NULL),
+(165, 151, 102, 3, 'Bollywood', 'video', 'Pal Pal Dil Ke Paas', '/assets/Music/Piano/palPalDilKePaas.mp4', NULL, NULL),
+(166, 201, 201, 8, 'Chudail Gufa', 'video', 'Chudail Gufa', '/assets/Stories/hindi/chudailGufa.mp4', NULL, NULL),
+(167, 202, 202, 7, 'FGHIJ', 'video', 'Humpty Dumpty', '/assets/Rhymes/humpty.mp4', NULL, NULL),
+(168, 7, 1, 6, 'Noun', 'video', 'Noun', '/assets/10/English/gr/noun.mp4', NULL, NULL),
+(169, 7, 1, 6, 'Noun', 'video', 'Collective and Abstract Noun', '/assets/10/English/gr/collectiveAndAbstractNoun.mp4', NULL, NULL),
+(170, 7, 1, 6, 'Narration', 'video', 'Direct and Indirect Speech', '/assets/10/English/gr/directIndirectSpeech.mp4', NULL, NULL),
+(171, 7, 1, 6, 'Pronoun', 'video', 'Introduction', '/assets/10/English/gr/pronoun.mp4', NULL, NULL),
+(172, 7, 1, 6, 'Conjunction', 'video', 'Introduction', '/assets/10/English/gr/conjunction.mp4', NULL, NULL),
+(173, 7, 1, 15, 'Letters', 'video', 'Formal Letter', '/assets/10/English/writing/formalLetter.mp4', NULL, NULL),
+(174, 7, 1, 15, 'Letter', 'video', 'Informal Letter', '/assets/10/English/writing/formalLetter.mp4', NULL, NULL),
+(175, 7, 1, 15, 'Notice', 'video', 'Introduction', '/assets/10/English/writing/notice.mp4', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -157,7 +233,11 @@ INSERT INTO `heads` (`sn`, `head`) VALUES
 (8, 'Hindi'),
 (9, 'History'),
 (10, 'Geography'),
-(11, 'Civics');
+(11, 'Civics'),
+(12, 'Physics'),
+(13, 'Chemistry'),
+(14, 'Biology'),
+(15, 'Writing');
 
 -- --------------------------------------------------------
 
@@ -220,6 +300,39 @@ INSERT INTO `sub` (`sub_code`, `sub_name`) VALUES
 (103, 'Harmonium'),
 (104, 'Tabla');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp`
+--
+
+CREATE TABLE `temp` (
+  `sn` int(11) NOT NULL DEFAULT '0',
+  `std_code` int(11) NOT NULL,
+  `sub_code` int(11) DEFAULT NULL,
+  `head` int(11) DEFAULT NULL,
+  `topic` text,
+  `type` text NOT NULL,
+  `name` text,
+  `path` text NOT NULL,
+  `start_vid` text,
+  `stop_vid` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `temp`
+--
+
+INSERT INTO `temp` (`sn`, `std_code`, `sub_code`, `head`, `topic`, `type`, `name`, `path`, `start_vid`, `stop_vid`) VALUES
+(0, 7, 1, 6, 'Noun', 'video', 'Noun', '/assets/10/English/gr/noun.mp4', NULL, NULL),
+(0, 7, 1, 6, 'Noun', 'video', 'Collective and Abstract Noun', '/assets/10/English/gr/collectiveAndAbstractNoun.mp4', NULL, NULL),
+(0, 7, 1, 6, 'Narration', 'video', 'Direct and Indirect Speech', '/assets/10/English/gr/directIndirectSpeech.mp4', NULL, NULL),
+(0, 7, 1, 6, 'Pronoun', 'video', 'Introduction', '/assets/10/English/gr/pronoun.mp4', NULL, NULL),
+(0, 7, 1, 6, 'Conjunction', 'video', 'Introduction', '/assets/10/English/gr/conjunction.mp4', NULL, NULL),
+(0, 7, 1, 15, 'Letters', 'video', 'Formal Letter', '/assets/10/English/writing/formalLetter.mp4', NULL, NULL),
+(0, 7, 1, 15, 'Letter', 'video', 'Informal Letter', '/assets/10/English/writing/formalLetter.mp4', NULL, NULL),
+(0, 7, 1, 15, 'Notice', 'video', 'Introduction', '/assets/10/English/writing/notice.mp4', NULL, NULL);
+
 --
 -- Indexes for dumped tables
 --
@@ -256,13 +369,13 @@ ALTER TABLE `sub`
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
-  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=183;
 
 --
 -- AUTO_INCREMENT for table `heads`
 --
 ALTER TABLE `heads`
-  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `sub`
--- includes/nav.php
+++ includes/nav.php
@@ -1,11 +1,14 @@
+<?php
+$compensation= "/color";
+?>
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb3" style="border-bottom:2px solid blue;">
 <div class="container">
-  <a href="#" class="navbar-brand"><b>COLOR - SCHOOL TERMINAL</b></a>
+  <a href="<?php echo $compensation;?>/class_sel.php" class="navbar-brand"><b>COLOR - SCHOOL TERMINAL</b></a>
   <button class="navbar-toggler" data-toggle="collapse" data-target="#menu"><span class="navbar-toggler-icon"></span></button>
   <div class="navbar-collapse collapse" id="menu">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
-        <a href="#" class="nav-link">Home</a>
+      <a href="<?php echo $compensation;?>/class_sel.php" class="nav-link">Home</a>
       </li>
       <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
